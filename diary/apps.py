3207aef22537b1fe07baead7922ca83ff603cdc3
kzk_maeda <casino.music0711@gmail.com>
2019-02-25 23:39:38 +0900

    model created

--- diary/apps.py
+++ diary/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class DiaryConfig(AppConfig):
     name = 'diary'
+
+# Create DynamoDB Table
+
+if not Day.exists():
+    Day.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,4 @@
+"""
 from django.db import models
 from django.utils import timezone
 
@@ -6,6 +7,23 @@ class Day(models.Model):
     title = models.CharField('タイトル', max_length=200)
     text = models.TextField('本文')
     date = models.DateTimeField('日付', default=timezone.now())
+
+    def __str__(self):
+        return self.title
+"""
+import uuid
+from pynamodb.models import Model
+from pynamodb.attributes import (
+    UnicodeAttribute, NumberAttribute, UnicodeSetAttribute, UTCDateTimeAttribute
+)
+from django.utils import timezone
+
+
+class Day(Model):
+    id = UnicodeAttribute(hash_key=True, default=str(uuid.uuid4()))
+    title = UnicodeAttribute(range_key=True)
+    text = UnicodeAttribute()
+    created_at = UTCDateTimeAttribute(default=timezone.now())
 
     def __str__(self):
         return self.title
