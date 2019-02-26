811197ace9e01619073ee98e86077bf72bbb3f81
kzk_maeda <casino.music0711@gmail.com>
2019-02-26 11:50:23 +0900

    basic feat is done

--- diary/apps.py
+++ diary/apps.py
@@ -7,17 +7,16 @@ import uuid
 class DiaryConfig(AppConfig):
     name = 'diary'
 
-# Create DynamoDB Table
+
+# Create DynamoDB Table & Save Initial Data
 if not Day.exists():
     Day.create_table(wait=True)
 
-# Save Initial Data
-print(Day.scan())
-save_data = Day(
-    str(uuid.uuid4()),
-    title="Sample Data 1",
-    text="this is sample data 1.",
-    created_at=timezone.now(),
-    updated_at=timezone.now()
-)
-save_data.save()
+    save_data = Day(
+        str(uuid.uuid4()),
+        title="Sample Data 1",
+        text="this is sample data 1.",
+        created_at=timezone.now(),
+        updated_at=timezone.now()
+    )
+    save_data.save()
