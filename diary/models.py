6dfdfe5e3db30381da07d904e9cc2bb53c3fbd32
kzk_maeda <casino.music0711@gmail.com>
2019-02-26 12:11:50 +0900

    dynamo db対応

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 import logging
 from pynamodb.models import Model
@@ -15,10 +16,12 @@ log.propagate = True
 class Day(Model):
     class Meta:
         table_name = "Day"
-        host = "http://localhost:8000"
+        # host = "http://localhost:8000"
         region = 'ap-northeast-1'
         write_capacity_units = 1
         read_capacity_units = 1
+        aws_access_key_id = os.environ.get('ACCESS_KEY')
+        aws_secret_access_key = os.environ.get('SECRET_ACCESS_KEY')
 
     id = UnicodeAttribute(hash_key=True, default=str(uuid.uuid4()))
     title = UnicodeAttribute()
